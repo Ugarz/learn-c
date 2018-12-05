a6385becb3bb3717161de84528b1094de3040cf7
Ugo <ugo.arzur@morning.com>
2018-12-05 14:17:20 +0100

    arrays

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ See [https://www.learn-c.org](https://www.learn-c.org/)
 # Workflow
 - [x] [Hello World](https://www.learn-c.org/en/Hello%2C_World%21)
 - [x] [Variables and Types](https://www.learn-c.org/en/Variables_and_Types)
-- [ ] [Arrays](https://www.learn-c.org/en/Arrays)
+- [x] [Arrays](https://www.learn-c.org/en/Arrays)
+- [ ] [Multidimensional Arrays](https://www.learn-c.org/en/Multidimensional_Arrays)
 
 # Run
 ```sh
--- core/main.c
+++ core/main.c
@@ -1,10 +1,23 @@
 // Enable printf
 #include <stdio.h>
 
-int main() {
-    int a = 0, b = 1, c = 2, d = 3, e = 4;
-    a = b - c + d * e;
-    printf("Good, Morning!");
-    printf("%d", a);
+// int main() {
+//     int a = 0, b = 1, c = 2, d = 3, e = 4;
+//     a = b - c + d * e;
+//     printf("Good, Morning!");
+//     printf("%d", a);
+//     return 0;
+// }
+
+int main(){
+    int numbers[10];
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+    numbers[5] = 60;
+    numbers[6] = 70;
+    printf("The 7th number in the array is %d", numbers[6]);
     return 0;
 }
